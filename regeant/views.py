9e34f946cffac247a6660d540c8ec0b2287dd8fa
Huang Chengwei <primecriminalkid@gmail.com>
2013-10-17 10:33:58 +0800

    修改搜索参数
    改排序为降序

--- regeant/views.py
+++ regeant/views.py
@@ -20,7 +20,7 @@ def search(request):
     keyword = request.GET.get('keyword')
     form = SearchForm({'keyword': keyword})
     query = Regeant.search.query(keyword)
-    results = query.order_by('@rank')
+    results = query.order_by('-@rank')
     producers = set()
     providers = set()
     for res in results:
