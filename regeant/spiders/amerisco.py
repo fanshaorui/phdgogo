9d14a8a5983e8cdf3064d8a77d27d9445b675a55
kid143 <primecriminalkid@gmail.com>
2013-10-19 19:28:57 +0800

    合并爬虫代码

--- regeant/spiders/amerisco.py
+++ regeant/spiders/amerisco.py
@@ -1,49 +1,46 @@
-from scrapy.http import Request
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from regeant.models import RegeantItem, Producer, Brand
+from .utils import HelperMixin
 
-class AmeriscoSpider(CrawlSpider):
-	name = 'amerisco'
-	allowed_domains = ['www.amresco-inc.com']
-	start_urls = [
-	#'http://www.amresco-inc.com/rna-loading-buffer-with-etbr-1b1607.cmsx',
-		'http://www.amresco-inc.com/home/products/products-home.cmsx'
-	]
 
-	rules =(
-		Rule(SgmlLinkExtractor(
-			allow=('/home/', ),deny=('.pdf', )),follow=True),
-		Rule(SgmlLinkExtractor(allow=()), callback='parse_item',follow=True)
-	)
-	def __init__(self):
-		self.producer = Producer.objects.get(name="AMRESCO")
-		self.brand = Brand.objects.get(name="AMRESCO")
-		CrawlSpider.__init__(self)
-	def join(self, group):
-		if len(group) > 0:
-			return ''.join(group).strip()
-		else:
-			return ""
-	def parse_item(self, response):
-		hxs = HtmlXPathSelector(response)
-		i = RegeantItem()
-		#???????????,????????????????
-		i['product_name'] = hxs.select('//div[@id="topleft"]/dl[2]/dd[1]/text()').extract()
-		i['product_english_name']=hxs.select('//h1/text()').extract()[1]
-		cas_no_sign=hxs.select('//div[@id="topleft"]/dl[1]/dt[1]/text()').extract()
-		if cas_no_sign=="CAS Number: ":
-			i['cas_no']=hxs.select('//div[@id="topleft"]/dl[1]/dd[1]/text()').extract()
-		else:
-			i['cas_no']=""
-		i['product_no'] = hxs.select('//h1/text()').extract()[0]
-		i['description'] = self.join(hxs.select('//div[@id="product_description"]').extract()) \
-				if hxs.select('//div[@id="product_description"]').extract() else ""
-		 
-		i['moleclar_structure_formation_path'] =""
-		i['producer'] = self.producer
-		i['brand'] = self.brand
-		i['url_path'] = response.url
-		i['original_html'] = response.body.strip()
-		return i
+class AmeriscoSpider(CrawlSpider, HelperMixin):
+    name = 'amerisco'
+    allowed_domains = ['www.amresco-inc.com']
+    start_urls = [
+    #'http://www.amresco-inc.com/rna-loading-buffer-with-etbr-1b1607.cmsx',
+        'http://www.amresco-inc.com/home/products/products-home.cmsx'
+    ]
+
+    rules = (
+        Rule(SgmlLinkExtractor(
+            allow=('/home/', ),deny=('.pdf', )),follow=True),
+        Rule(SgmlLinkExtractor(allow=()), callback='parse_item',follow=True)
+    )
+
+    def __init__(self):
+        self.producer = Producer.objects.get(name="AMRESCO")
+        self.brand = Brand.objects.get(name="AMRESCO")
+        CrawlSpider.__init__(self)
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        i = RegeantItem()
+        i['product_name'] = hxs.select('//div[@id="topleft"]/dl[2]/dd[1]/text()').extract()
+        i['product_english_name'] = hxs.select('//h1/text()').extract()[1]
+        cas_no_sign = hxs.select('//div[@id="topleft"]/dl[1]/dt[1]/text()').extract()
+        if cas_no_sign == "CAS Number: ":
+            i['cas_no'] = hxs.select('//div[@id="topleft"]/dl[1]/dd[1]/text()').extract()
+        else:
+            i['cas_no'] = ""
+        i['product_no'] = hxs.select('//h1/text()').extract()[0]
+        i['description'] = self.join(hxs.select('//div[@id="product_description"]').extract()) \
+                if hxs.select('//div[@id="product_description"]').extract() else ""
+
+        i['moleclar_structure_formation_path'] = ""
+        i['producer'] = self.producer
+        i['brand'] = self.brand
+        i['url_path'] = response.url
+        i['original_html'] = response.body.strip()
+        return i
--- regeant/spiders/cellsignal.py
+++ regeant/spiders/cellsignal.py
@@ -1,51 +1,48 @@
-
-from scrapy.http import Request
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from regeant.models import RegeantItem, Producer, Brand
-class CellsignalSpider(CrawlSpider):
-	name = 'cellsignal'
-	allowed_domains = ['www.cellsignal.com']
-	start_urls = [
-	#'http://www.cellsignal.com/products/12046.html',
-		'http://www.cellsignal.com/catalog/index.html'
-	]
+from .utils import HelperMixin
 
-	rules =(
-		Rule(SgmlLinkExtractor(
-			allow=('/products/', )), callback='parse_item'),
-		Rule(SgmlLinkExtractor(allow=()), follow=True)
-	)
-	def __init__(self):
-		self.producer = Producer.objects.get(name="CST")
-		self.brand = Brand.objects.get(name="CST")
-		CrawlSpider.__init__(self)
-	def join(self, group):
-		if len(group) > 0:
-			return ''.join(group).strip()
-		else:
-			return ""
-	def parse_item(self, response):
-		hxs = HtmlXPathSelector(response)
-		i = RegeantItem()
-		i['product_name'] = ""
-		product_english_name=self.join(hxs.select('//h1[@class="productName"]/text()').extract())
-		s=product_english_name.find("#")
-		i['product_english_name'] = product_english_name[0:s]
-		i['cas_no']=""
-		product_no = response.url
-		if product_no:
-			product_no=product_no.encode("gbk")
-			product_no = filter(str.isdigit,product_no)
-		i['product_no'] = product_no
-		i['description'] = self.join(hxs.select('//div[@id="background"]/p/text()').extract()) \
-				if hxs.select('//div[@id="background"]/p/text()').extract() else ""
-		i['moleclar_structure_formation_path'] = self.join(
-			hxs.select('//div[@id="product-images"]/div[1]/a/@href').extract()) \
-				if hxs.select('//div[@id="product-images"]/div[1]/a/@href').extract() else ""
-		i['producer'] = self.producer
-		i['brand'] = self.brand
-		i['url_path'] = response.url
-		i['original_html'] = response.body.strip()
-		return i
+
+class CellsignalSpider(CrawlSpider, HelperMixin):
+    name = 'cellsignal'
+    allowed_domains = ['www.cellsignal.com']
+    start_urls = [
+        'http://www.cellsignal.com/catalog/index.html'
+    ]
+
+    rules = (
+        Rule(SgmlLinkExtractor(
+            allow=('/products/', )), callback='parse_item'),
+        Rule(SgmlLinkExtractor(allow=()), follow=True)
+    )
+
+    def __init__(self):
+        self.producer = Producer.objects.get(name="CST")
+        self.brand = Brand.objects.get(name="CST")
+        CrawlSpider.__init__(self)
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        i = RegeantItem()
+        i['product_name'] = ""
+        product_english_name = self.join(hxs.select('//h1[@class="productName"]/text()').extract())
+        s = product_english_name.find("#")
+        i['product_english_name'] = product_english_name[0:s]
+        i['cas_no'] = ""
+        product_no = response.url
+        if product_no:
+            product_no=product_no.encode("gbk")
+            product_no = filter(str.isdigit, product_no)
+        i['product_no'] = product_no
+        i['description'] = self.join(hxs.select('//div[@id="background"]/p/text()').extract()) \
+                if hxs.select('//div[@id="background"]/p/text()').extract() else ""
+        i['moleclar_structure_formation_path'] = self.join(
+            hxs.select('//div[@id="product-images"]/div[1]/a/@href').extract()) \
+                if hxs.select('//div[@id="product-images"]/div[1]/a/@href').extract() else ""
+        i['producer'] = self.producer
+        i['brand'] = self.brand
+        i['url_path'] = response.url
+        i['original_html'] = response.body.strip()
+        return i
