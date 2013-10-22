2afb474c9bb4e7c6b01aaa29284b3856e9a66b51
fanshaorui <fanshaorui@gmail.com>
2013-10-22 18:55:40 +0800

    10.22优化体验
    1、新增供应商搜索结果页,将供应商联系方式提出来
    2、新增隐私权政策页面
    3、优化搜索结果页表现

--- search_demo/urls.py
+++ search_demo/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -14,16 +15,18 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^search/', 'regeant.views.search'),
-    url(r'^product/(\w+)/', 'regeant.views.detail'),
-    url(r'^provider/$', 'providers.views.index'),
-    url(r'^provider/info/$','providers.views.providerinfo'),
-    url(r'^accounts/logout/$','providers.views.logout_view'),
-    url(r'^accounts/login_page/$','providers.views.login_page'),
-    url(r'^providers/register/$','providers.views.register'),
-    url(r'customer/cart/view/','customer.views.cart_view'),
-    url(r'^customer/cart/add/(\d+)/$','customer.views.add_cart'),
-    url(r'customer/cart/clean/','customer.views.clean_cart'),
-    url(r'customer/cart/excel/','customer.views.excelview'),
-    url(r'^$', 'regeant.views.index'),
+    url(r'^private/$','regeant.views.private'),#隐私权政策
+    url(r'search/', 'regeant.views.search'),#搜索结果页
+    url(r'providerdetail/(\w+)/', 'regeant.views.provider_detail'),#产品供应商详情页
+    url(r'propro/','regeant.views.producers_provider_all'),#
+    url(r'^provider/$', 'providers.views.index'),#供应商后台首页
+    url(r'^provider/info/$','providers.views.providerinfo'),#供应商信息页
+    url(r'^accounts/logout/$','providers.views.logout_view'),#退出登陆
+    url(r'^accounts/login_page/$','providers.views.login_page'),#供应商登陆
+    url(r'^providers/register/$','providers.views.register'),#供应商注册
+    url(r'customer/cart/view/','customer.views.cart_view'),#用户采购清单
+    url(r'^customer/cart/add/(\d+)/$','customer.views.add_cart'),#采购清单添加
+    url(r'customer/cart/clean/','customer.views.clean_cart'),#清空采购清单
+    url(r'customer/cart/excel/','customer.views.excelview'),#采购清单导出excel
+    url(r'', 'regeant.views.index'),#首页
 )
