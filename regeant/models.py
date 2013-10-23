5b43cc50e356cc423020f44d6c3f2ef204887bdf
Huang Chengwei <primecriminalkid@gmail.com>
2013-10-23 08:27:31 +0800

    调整代码格式

--- regeant/models.py
+++ regeant/models.py
@@ -17,7 +17,9 @@ class Producer(models.Model):
     logo_path = models.URLField(blank=True)
     country = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
-    providers=models.ManyToManyField(Provider,related_name="provider_relation_1")
+    providers = models.ManyToManyField(
+        Provider, related_name="provider_relation_1")
+
     def __unicode__(self):
         return self.name
 
@@ -92,24 +94,32 @@ class Regeant(models.Model):
     def product_url(self):
         ''' property method for results page href attributes'''
         return "/product/%s.html" % self.id
+
     @classmethod
-    def page(cls, query_str, page_num=1, per_page=20,producer_id=None):
+    def page(cls, query_str, page_num=1, per_page=20, producer_id=None):
         """return Pagination object"""
         query = cls.search.query(query_str)
         query = query.order_by('-@rank', mode=sphapi.SPH_SORT_EXTENDED)
         if producer_id:
-            query=query.filter(producer_id=producer_id)
+            query = query.filter(producer_id=producer_id)
         return Pagination(query, page_num)
+
     def __unicode__(self):
         return self.product_name if self.product_name \
             else self.product_english_name
 
+
 class emailinfo(models.Model):
-    email=models.CharField(max_length=200)
-    url=models.URLField()
+    email = models.CharField(max_length=200)
+    url = models.URLField()
+
     def __unicode__(self):
         return self.email
+
+
 class RegeantItem(DjangoItem):
     django_model = Regeant
+
+
 class emailItem(DjangoItem):
-    django_model=emailinfo
+    django_model = emailinfo
--- regeant/views.py
+++ regeant/views.py
@@ -6,7 +6,7 @@
 from django.core.context_processors import csrf
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
-from .models import Regeant,Producer
+from .models import Regeant, Producer
 from .forms import SearchForm
 
 
@@ -16,7 +16,7 @@ def index(request):
         request, {'form': form}))
 
 
-def search(request,target_producer=None):
+def search(request, target_producer=None):
     keyword = request.GET.get('keyword')
     form = SearchForm({'keyword': keyword})
     page_num = int(request.GET.get('page_num', 1))
@@ -26,34 +26,40 @@ def search(request,target_producer=None):
     for res in pagination_original.cur_page:
         producers.add(res.producer)
         providers.update(res.producer.providers.all())
-    pagination = Regeant.page(keyword, page_num=page_num,producer_id=target_producer)
-    producers_in_session=[]
+    pagination = Regeant.page(
+        keyword, page_num=page_num, producer_id=target_producer)
+    producers_in_session = []
     for producer in producers:
-	producers_in_session.append(producer.id)
-    request.session["producers_in_session"]=producers_in_session
+        producers_in_session.append(producer.id)
+        request.session["producers_in_session"] = producers_in_session
     return render_to_response("results.html", RequestContext(
         request, {
             'form': form,
             'keyword': keyword,
             'results': pagination,
             'producer_quant': len(producers),
-	    'producers':producers,
+            'producers': producers,
             'provider_quant': len(providers),
             'res_num': pagination.total}))
 
+
 def producers_provider_all(request):
-    producers=[]
-    producer_ids=request.session.get("producers_in_session",None)
+    producers = []
+    producer_ids = request.session.get("producers_in_session", None)
     if producer_ids:
-	for id in producer_ids:
-	    producer=Producer.objects.get(id=id)
-	    producers.append(producer)
-    c=RequestContext(request,locals())
+        for id in producer_ids:
+            producer = Producer.objects.get(id=id)
+            producers.append(producer)
+    c = RequestContext(request, locals())
     return render_to_response("provideralldetail.html", c)
-def provider_detail(request,producer_pk):
+
+
+def provider_detail(request, producer_pk):
     #print product_pk
-    producer = get_object_or_404(Producer,pk=producer_pk)
-    c=RequestContext(request,locals())
+    producer = get_object_or_404(Producer, pk=producer_pk)
+    c = RequestContext(request, locals())
     return render_to_response("providerdetail.html", c)
+
+
 def private(request):
-    return render_to_response("private.html",RequestContext(request,dict()))
+    return render_to_response("private.html", RequestContext(request, dict()))
